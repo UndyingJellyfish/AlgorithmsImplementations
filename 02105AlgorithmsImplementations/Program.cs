2ebe3bb0e49850dffc13c2bed1df8f417a436f0c
s164395 <s164395@student.dtu.dk>
2017-04-06 09:39:05 +0200

    Add project files.

---
+++ 02105AlgorithmsImplementations/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.Threading.Tasks;
+
+namespace _MainNamespace
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Let us create the following graph
+               2    3
+            (0)--(1)--(2)
+            |    / \   |
+           6|  8/   \5 |7
+            |  /     \ |
+            (3)-------(4)
+                 9          
+            */
+
+            int V = 5;
+             
+            int[,] graph = new int[,] {{0, 2, 0, 6, 0},
+                                    {2, 0, 3, 8, 5},
+                                    {0, 3, 0, 0, 7},
+                                    {6, 8, 0, 0, 9},
+                                    {0, 5, 7, 9, 0},
+                                   };
+            MinimumSpanningTrees.PrimsAlgorithm.primMST(graph, V);
+
+
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
