087f9c104080cc96011fea0b598f0533d0322261
s164395 <s164395@student.dtu.dk>
2017-08-04 12:48:51 +0200

    Probably works

---
+++ Trees/Node.cs
@@ -0,0 +1,20 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trees
+{
+    class Node
+    {
+        private int key { get; set; }
+        private Object value { get; set; }
+        private Node parent { get; set; }
+        private Node left { get; set; }
+        private Node right { get; set; }
+        private List<Node> children { get; set; }
+
+    }
+}
