62f5818295f23a14dcae148b08dc66f91fe34f65
Emil Damsbo <s164395@student.dtu.dk>
2018-05-06 23:58:48 +0200

    Wrote much needed comments and docs for RadixSort.cs

--- Sorting/RadixSort.cs
+++ Sorting/RadixSort.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using Byte;
 using Utilities;
 
 namespace Sorting
@@ -11,21 +12,22 @@ namespace Sorting
         public static List<int> Sort(List<int> ints)
         {
             var intsBuckets = new List<List<int>>(Base);
-
             for (var i = 0; i < Base; i++) // init buckets
             {
                 intsBuckets.Add(new List<int>());
             }
-            var max = ints.Concat(new[] { 0 }).Max();
+            var max = ints.Concat(new[] { 0 }).Max(); // find maximum value in list
 
             for (var power = 1; max / power != 0; power = power * Base)
             {
                 foreach (var val in ints)
                 {
-                    intsBuckets[(val / power) % Base].Add(val);
+                    // put val into a certain bucket dependent on the value of the given number
+                    intsBuckets[val / power % Base].Add(val);
                 }
-                var index = 0;
 
+                var index = 0;
+                // intermittently sort partial list after putting new things into buckets
                 foreach (var bucket in intsBuckets)
                 {
                     foreach (var val in bucket)
@@ -34,7 +36,6 @@ namespace Sorting
                     }
                 }
             }
-
             return ints;
         }
     }
@@ -43,38 +44,62 @@ namespace Sorting
         private const int Base = 10;
         private Calculator<T> Calculator { get; set; }
         private Calculator<T> C => Calculator;
-        private static T Generify(int val) => GenericConversions<T>.Generify(val);
 
+        /// <summary>
+        /// Generifies the specified value to any of the integral or floating point types.
+        /// </summary>
+        /// <param name="val">The value to be "generified".</param>
+        /// <returns>A value of <c>T</c>.</returns>
+        private static T Generify(int val) => GenericConversions<T>.Generify(val);
+        private T Add(object val1, object val2) => C.Add((T)val1, GenericConversions<T>.Generify(val2));
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RadixSort{T}"/> class. 
+        /// It is very important that the calculator parameter corresponds to the type <c>T</c>, 
+        /// otherwise none of the arithmetic functions performed by the sorting algorithm is 
+        /// guaranteed to work as intended, resulting in uncertain sorting results.
+        /// </summary>
+        /// <param name="calc">The calculator for generic arithmetic.</param>
         public RadixSort(Calculator<T> calc)
         {
             this.Calculator = calc;
         }
 
+
+        /// <summary>
+        /// Sorts the specified list.
+        /// </summary>
+        /// <param name="list">The list to sort.</param>
+        /// <returns>A sorted list of type <c>T</c>, where <c>T</c> is given by the type parameter.</returns>
+        /// <exception cref="NotImplementedException">Radix sort with float, double or decimal is not yet supported, use another sorting algorithm.</exception>
         public List<T> Sort(List<T> list)
         {
             // TODO: This might help: https://stackoverflow.com/questions/2685035/is-there-a-good-radixsort-implementation-for-floats-in-c-sharp
+            // otherwise this might: https://webcache.googleusercontent.com/search?q=cache:DPuhAHY36CAJ:https://gist.github.com/StagPoint/44aebfb6960c3f5fdd6f25862827ec43+&cd=4&hl=en&ct=clnk&gl=dk
             if (typeof(T) == typeof(float) || typeof(T) == typeof(double) || typeof(T) == typeof(decimal))
             {
                 throw new NotImplementedException("Radix sort with float, double or decimal is not yet supported, use another sorting algorithm.");
             }
 
             var buckets = new List<List<T>>(Base);
-
-            for (var i = 0; i < Base; i++)
+            for (var i = 0; i < Base; i++) // initialize buckets
             {
                 buckets.Add(new List<T>());
             }
-            var max = list.Concat(new[] { default(T) }).Max();
+            var max = list.Concat(new[] { default(T) }).Max(); //find maximum value in list
 
             for (var pow = Add(default(T), 1); !C.Div(max, pow).Equals(Generify(0)); pow = C.Mult(pow, Generify(Base)))
             {
                 foreach (var val in list)
                 {
-                    buckets[(int)Convert.ChangeType(C.Div(val, pow), typeof(int)) % Base].Add(val);
+                    // put val into a certain bucket dependent on the value of the given number
+                    var tempIndex = (int) Convert.ChangeType(C.Div(val, pow), typeof(int)) % Base;
+                    buckets[tempIndex].Add(val);
                 }
-                var index = 0;
 
-                foreach (var bucket in buckets)
+                var index = 0;
+                // intermittently sort partial list after putting new things into buckets
+                foreach (var bucket in buckets) 
                 {
                     foreach (var val in bucket)
                     {
@@ -82,13 +107,7 @@ namespace Sorting
                     }
                 }
             }
-
             return list;
-        }
-
-        private T Add(object val1, object val2)
-        {
-            return C.Add((T)val1, GenericConversions<T>.Generify(val2));
         }
     }
 }
