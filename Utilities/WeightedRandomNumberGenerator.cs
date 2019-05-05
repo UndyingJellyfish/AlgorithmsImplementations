1d35192b44d1df3cfbbeb4070127d0903551d959
Emil Damsbo <s164395@student.dtu.dk>
2019-05-06 00:30:21 +0200

    Add cryptographically safe weighted RNG

---
+++ Utilities/WeightedRandomNumberGenerator.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Linq;
+using System.Security.Cryptography;
+
+namespace DamsboSoftware.AlgorithmImplementations.Utilities
+{
+    public class WeightedRandomNumberGenerator
+    {
+        public int[] choiceWeights { get; set; }
+
+        public WeightedRandomNumberGenerator()
+        {
+            this.choiceWeights = new int[0];
+        }
+
+        public WeightedRandomNumberGenerator(int[] weights) : this()
+        {
+            this.choiceWeights = weights;
+        }
+
+        public long NextLong()
+        {
+
+            long sumOfWeights = choiceWeights.Sum();
+            long rnd = LongRandom(0, sumOfWeights);
+            for (var i = 0; i < choiceWeights.Length; i++)
+            {
+                if (rnd < choiceWeights[i])
+                    return i;
+                rnd -= choiceWeights[i];
+            }
+
+            throw new ArgumentException();
+        }
+
+        private long LongRandom(long min, long max)
+        {
+            if (min > max) throw new ArgumentOutOfRangeException(nameof(min));
+            if (min == max) return min;
+
+            using (var rngCrypto = new RNGCryptoServiceProvider())
+            {
+                var data = new byte[8];
+                rngCrypto.GetBytes(data);
+
+                var generatedValue = Math.Abs(BitConverter.ToInt64(data, startIndex: 0));
+
+                var diff = max - min;
+                var mod = generatedValue % diff;
+
+                return min + mod;
+            }
+        }
+    }
+}
